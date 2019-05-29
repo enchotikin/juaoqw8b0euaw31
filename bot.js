1077afa3d91806176d9a97905fa84e71713f5e14
enchotikin <51168033+enchotikin@users.noreply.github.com>
2019-05-30 00:06:37 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+require('./util/eventLoader')(client);
+
+var prefix = ayarlar.prefix;
+
+client.on("guildMemberAdd", member => {
+
+	var channel = member.guild.channels.find("name", "gelen-giden");
+	if (!channel) return;
+
+	var role = member.guild.roles.find("name", "😘 Yıldırım Ailesi 😘");
+	if (!role) return;
+
+	member.addRole(role);
+
+	channel.send(member + " artık " + role + " rolü ile aramızda");
+
+	member.send("Aramıza hoş geldin! Artık @😘 Yıldırım Ailesi 😘 rolüne sahipsin!")
+
+});
+
+client.on('guildBanAdd' , (guild, user) => {
+  let Banyiyenler = guild.channels.find('name', 'ban-yiyenler');
+  if (!Banyiyenler) return;
+  Banyiyenler.send('https://media.giphy.com/media/8njotXALXXNrW/giphy.gif **Adalet dağıtma zamanı gelmiş!**  '+ user.username +  ' **Aileden Kovuldun Güle Güle** :fist: :writing_hand:  :spy:' );
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+      msg.reply(' :heart: Aleyküm Selam Yıldırım Ailesine Hoşgeldin :heart: !');
+}
+});
+
+client.login("NTgzMzk0NTE5NjE0MDk1NDY4.XO7ujg.TLg-CZzY9QYnkHhe0PvtUG5-ep0");
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('Değerli **' + username + '** Yıldırım Ailesine Hoşgeldin :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle https://gph.is/2VhWm6k :wink:');
+
+};
